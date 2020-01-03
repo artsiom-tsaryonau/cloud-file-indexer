3672b09ecde36f31bd974ea9340759fc6dc7431b
artsiom-tsaryonau <artemii1992@gmail.com>
2020-01-03 09:16:11 +0100

    Refactoring for search

--- cloud.py
+++ cloud.py
@@ -6,39 +6,33 @@ SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
 class CloudClientFactory:
     def get_client(self, type, *constructor_args):
-        if type == 'gdrive 1':
+        if type == 'gdrive':
             return GDriveClientWrapper(constructor_args[0])
         elif type == 'mega':
             pass
 
-class GDriveClientWrapper:
-    def __init__(self, credentials_file):
-        self.creds = service_account.Credentials.from_service_account_file(
-            credentials_file, scopes=SCOPES)
-        self.client = build('drive', 'v3', credentials=self.creds)
-
-    # returns list as there might be multiple folders with the same name
-    def __find_folder_ids(self, folder):
-        results = self.__do_search(
-            "mimeType='application/vnd.google-apps.folder' and name='" + folder + "'", 
-            'nextPageToken, files(id, name)') 
-        return [result["id"] for result in results]
-    
-    # TODO: implement recursive search
-    def __list_files(self, folder, types=None):
-        folder_ids = self.__find_folder_ids(folder)
-        all_files = []
-        for folder_id in folder_ids:
-            # ugly
-            if types is None:
-                query="parents in '" + folder_id + "'"
-            else:
-                types_str = ' or '.join(["mimeType='" + type + "'" for type in types])
-                query=types_str + " and parents in '" + folder_id + "'"
-            all_files += self.__do_search(query, 'nextPageToken, files(id, name, mimeType)')
-        return all_files
+class GDriveQuery:
+    def __init__(self, mime_types, name=None, parents=None):
+        self.mime_types = mime_types
+        self.name = name
+        self.parents = parents
         
-    def __do_search(self, query, fields):
+    def build(self):
+        full_filter = []
+        full_filter += ' or '.join(["mimeType='" + type + "'" for type in mime_types])
+        if parents:
+            full_filter += ' and '.join(["'" + parent_id + "' in parents" for parent_id in parents]) 
+        if name:
+            full_filter += "name='" + name + "'"
+        query = ' and '.join(full_filter)
+        print(query)
+        return query
+      
+class GDriveIndexer:
+    def __init__(self, client):
+        self.client = client
+        
+    def do_search(self, query, fields):
         results = []
         page_token = None
         while True:
@@ -52,6 +46,26 @@ class GDriveClientWrapper:
                 break
         return results
 
+class GDriveClientWrapper:
+    def __init__(self, credentials_file):
+        self.indexer = GDriveIndexer(build('drive', 'v3', 
+            credentials=service_account.Credentials.from_service_account_file(credentials_file, scopes=SCOPES))
+
+    # returns list as there might be multiple folders with the same name
+    def __find_folder_ids(self, folder):
+        query_builder = GDriveQuery(['application/vnd.google-apps.folder'], folder)
+        results = self.indexer.do_search(query_builder.build(), 'nextPageToken, files(id, name)') 
+        return [result["id"] for result in results]
+    
+    # TODO: implement recursive search
+    def __list_files(self, folder, types=None):
+        folder_ids = self.__find_folder_ids(folder)
+        all_files = []
+        for folder_id in folder_ids:
+            query_builder = GDriveQuery(types, parents=[folder_id])
+            all_files += self.indexer.do_search(query_builder.build(), 'nextPageToken, files(id, name, mimeType)')
+        return all_files
+        
     def list_files(self, folder, types):
         return [file['name'] for file in self.__list_files(folder, types)]
         
